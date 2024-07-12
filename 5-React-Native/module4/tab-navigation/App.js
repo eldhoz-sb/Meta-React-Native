109af262aa4214059e62d23f617425a0768f4f5a
eldhoz-sb <110928118+eldhoz-sb@users.noreply.github.com>
2024-07-12 13:42:10 +0530

    commit after implementing tab navigation in  module4

---
+++ 5-React-Native/module4/tab-navigation/App.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './WelcomeScreen';
+import LoginScreen from './LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Tab.Navigator initialRouteName="Login" screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if(route.name === "Login"){
+                iconName = focused?'ios-enter':'ios-enter-outline'
+              }
+              else if(route.name === "Welcome") {
+                iconName = 'ios-home';
+              }
+              return <Ionicons name={iconName} color={color} size={size} />
+            }
+          })}>
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Login" component={LoginScreen} />
+          </Tab.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
+
